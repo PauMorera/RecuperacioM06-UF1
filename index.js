20a76175e4d7ec567e058adb88317d60eadbfcea
PauMorera <pmorera847dlc@gmail.com>
2023-06-16 10:12:49 +0200

    Crear Cookie

---
+++ index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('cookieForm').onsubmit = function(e) {
+      e.preventDefault();
+      crearCookie();
+  }
+
+  refreshCookies();
+});
+function crearCookie() {
+  var nom = document.getElementById('nom').value;
+  var valor = document.getElementById('valor').value;
+  var expiracio = new Date(document.getElementById('expiracio').value);
+  document.cookie = nom + "=" + valor + "; expires=" + expiracio.toUTCString() + "; path=/";
+  refreshCookies();
+}
+function refreshCookies() {
+  var cookies = document.cookie.split('; ');
+  var cookiesContainer = document.getElementById('cookiesContainer');
+  cookiesContainer.innerHTML = '';
+  for(var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].split('=');
+      displayCookie(cookie[0], cookie[1]);
+  }
+}
